1f97adf36506e6412213e8006d0946cc6f06a20a
Camryn Friedman <camryn.friedman@avanade.com>
2023-09-28 12:41:23 -0400

    Adding files

---
+++ demo/src/main/java/com/example/demo/JavaSpringPersonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPersonalApplication.class, args);
+	}
+
+}
